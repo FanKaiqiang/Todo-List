47efebe5de761d9f7f398d23351f85eeb6a4577e
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-18 16:42:02 +0800

    清空项目

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-import j from 'jquery'
-function component() {
-  var element = j('<div></div>');
-  element.html(_.join(['Hello', 'webpack'], ' '))
-  return element.get(0);
-}
-
-document.body.appendChild(component());
