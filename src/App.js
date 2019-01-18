280a21b7e43d11eeb7ddff413cb38621fbb9f420
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-18 20:12:15 +0800

    基本架构

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        { id: 1, title: '第一条待办事项' }
+      ]
+    }
+  }
   render() {
+    let todos = this.state.todoList.map((item)=>{
+      return <li>{item.title}</li>
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>待办事项</h1>
+        <div className="App-header">
+          <input type="text" value={this.state.newTodo} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
       </div>
-    );
+    )
+
   }
 }
 
