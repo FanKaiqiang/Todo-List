1d83bcd817a0af6cf77a8ef5241ea6f07f7353d0
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-19 20:19:17 +0800

    错误提示信息

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {signUp,signIn} from './leanCloud'
+import { signUp, signIn } from './leanCloud'
 import './UserDialog.scss'
 
 export default class UserDialog extends Component {
@@ -13,7 +13,7 @@ export default class UserDialog extends Component {
       }
     }
   }
-  switch = (e) => {
+  switch = (e) => {//切换状态清空输入框
     this.setState({
       selected: e.target.value,
       formData: {
@@ -22,32 +22,47 @@ export default class UserDialog extends Component {
       }
     })
   }
-  signUp = (e) => {
+  signUp = (e) => {//注册按键，将填入内容调用注册API
     e.preventDefault()
-    let {username,password} = this.state.formData
-    let success = (user) =>{
+    let { username, password } = this.state.formData
+    if (!username || !password) {
+      alert('用户名或密码不得为空')
+      return
+    }
+    let success = (user) => {
       this.props.stateUpdate.call(null, user)
     }
-    let error = (error) =>{
-      console.log(error)
+    let error = (error) => {
+      switch (error.code) {
+        case 202:
+          alert('用户名已被占用')
+          break
+        default:
+          alert(error)
+          break
+      }
     }
-    signUp(username,password,success,error)
+    signUp(username, password, success, error)
   }
-  signIn = (e) => {
+  signIn = (e) => {//登录按键，将填入内容调用登录API
     e.preventDefault()
-    let {username,password} = this.state.formData
-    let success = (user) =>{
+    let { username, password } = this.state.formData
+    if (!username || !password) {
+      alert('用户名或密码不得为空')
+      return
+    }
+    let success = (user) => {
       this.props.stateUpdate.call(null, user)
     }
-    let error = (error) =>{
-      console.log(error)
+    let error = (error) => {
+      alert('用户名与密码不匹配，请检查用户名或密码是否填写正确')
     }
-    signIn(username,password,success,error)
+    signIn(username, password, success, error)
   }
-  changeFormData = (e) => {
+  changeFormData = (e) => {//输入框内容更新时，更新state.fromData
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     if (e.target.type === 'text') {
-      stateCopy.formData.username = e.target.value
+      stateCopy.formData.username = e.target.value.trim()
     } else {
       stateCopy.formData.password = e.target.value
     }
