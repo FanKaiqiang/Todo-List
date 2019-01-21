f787e6b842f6ebeb7817e870d46363195756b1f0
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-21 11:42:02 +0800

    consturctor时发起getByUser

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -8,7 +8,7 @@ AV.init({
   appKey: APP_KEY
 })
 
-function getUserFromAVUser(AVUser){
+function getUserFromAVUser(AVUser) {
   return {
     id: AVUser.id,
     ...AVUser.attributes
@@ -17,8 +17,43 @@ function getUserFromAVUser(AVUser){
 
 export default AV
 
-export function signUp(username, password,email, successFn, errorFn){//leancloud的用户注册API
-   // 新建 AVUser 对象实例
+export const TodoModel = {// 所有跟 Todo 相关的 LeanCloud 操作都放到这里
+  getByUser(user, successFn, errorFn) {
+    // 文档见 https://leancloud.cn/docs/leanstorage_guide-js.html#批量操作
+    let query = new AV.Query('Todo')
+    query.find().then((response) => {
+      let array = response.map((t) => {
+        return { id: t.id, ...t.attributes }
+      })
+      successFn.call(null, array)
+    }, (error) => {
+      errorFn && errorFn.call(null, error)
+    })
+  },
+  create({ status, title, deleted }, successFn, errorFn) {
+    let Todo = AV.Object.extend('Todo') //建立表，设置值
+    let todo = new Todo()
+    todo.set('title', title)
+    todo.set('status', status)
+    todo.set('deleted', deleted)
+    todo.save().then(function (response) {
+      console.log(response)
+      successFn.call(null, response.id)
+    }, function (error) {
+      errorFn && errorFn.call(null, error)
+    });
+
+  },
+  update() {
+
+  },
+  destroy() {
+
+  }
+}
+
+export function signUp(username, password, email, successFn, errorFn) {//leancloud的用户注册API
+  // 新建 AVUser 对象实例
   var user = new AV.User()
   // 设置用户名
   user.setUsername(username)
@@ -34,30 +69,30 @@ export function signUp(username, password,email, successFn, errorFn){//leancloud
   })
 }
 
-export function signIn(username, password, successFn, errorFn){//leancloud的登录注册API
- AV.User.logIn(username, password).then(function (loginedUser) {//注册
-   let user = getUserFromAVUser(loginedUser)
-   successFn.call(null, user)//成功回调
- }, function (error) {
-   errorFn.call(null, error)//失败回调
- })
+export function signIn(username, password, successFn, errorFn) {//leancloud的登录注册API
+  AV.User.logIn(username, password).then(function (loginedUser) {//注册
+    let user = getUserFromAVUser(loginedUser)
+    successFn.call(null, user)//成功回调
+  }, function (error) {
+    errorFn.call(null, error)//失败回调
+  })
 }
 
-export function getCurrentUser(){//获取当前缓存的用户信息
+export function getCurrentUser() {//获取当前缓存的用户信息
   let user = AV.User.current()
-  if(user){
+  if (user) {
     return getUserFromAVUser(user)
   }
 }
 
-export function signOut(){//登出操作
+export function signOut() {//登出操作
   AV.User.logOut()
 }
 
-export function sendPasswordResetEmail(email, successFn, errorFn){
+export function sendPasswordResetEmail(email, successFn, errorFn) {
   AV.User.requestPasswordReset(email).then(function (success) {
-    successFn.call(null,success) 
+    successFn.call(null, success)
   }, function (error) {
-    errorFn.call(null,error)
+    errorFn.call(null, error)
   })
 }
