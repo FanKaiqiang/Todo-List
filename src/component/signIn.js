cc2eda8ee63ba905143c2409110fb7860db5aab9
FanKaiqiang <fankaiqiang001@gmail.com>
2019-02-02 18:00:31 +0800

    重构登入与登出界面

--- src/component/signIn.js
+++ src/component/signIn.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
-export default function SignIn (props){
+export default function SignIn(props) {
+  return (
+    <form className="signIn" onSubmit={props.onSubmit}>
 
-    return(
-      <form className="signIn" onSubmit={props.onSubmit}>
-        <div className="row">
-          <label>用户名</label>
-          <input type="text" value={props.formData.username} onChange={props.onChange} />
-        </div>
-        <div className="row">
-          <label>密码</label>
-          <input type="password" value={props.formData.password} onChange={props.onChange} />
-        </div>
-        <div className="row actions">
-          <button type="submit">登录</button>
-          <span onClick={props.onForgotPassword}>忘记密码了？</span>
-        </div>
-      </form>
-    )
-  
+      <div className="title">登录你的 To-Do List 账户</div>
+      <div className="ask">登录你的账户，然后就可以开始管理待办事项了</div>
+      <div className="row">
+        <label>用户名</label>
+        <input type="text" value={props.formData.username} onChange={props.onChange} />
+      </div>
+      <div className="row">
+        <label>密码</label>
+        <input type="password" value={props.formData.password} onChange={props.onChange} />
+      </div>
+      <div className="resetPassword">忘记密码了？<span onClick={props.onForgotPassword}>重置密码</span></div>
+      <button className="back" type="button" onClick={props.back}>上一步</button>
+      <button className="continue" type="submit">继续</button>
+    </form>
+  )
+
 }
