85c967b18aecb0a17528bcc0d9f58fa68f800b14
FanKaiqiang <fankaiqiang001@gmail.com>
2019-02-03 18:36:49 +0800

    三种事项状态界面的切换

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ export default class App extends Component {
     this.id = 0
     this.state = {
       user: getCurrentUser() || {},
-      newTodo: 'test',
-      todoList: []
+      newTodo: '',
+      todoList: [],
+      option: 'doing'
     }
     let user = getCurrentUser()
     if (user) {
@@ -25,20 +26,28 @@ export default class App extends Component {
     }
   }
 
+
+
   idMaker() {
     return this.id++
   }
 
   addTodo = (event) => {//传递给input的方法，event为input元素
+    if (event.target.value.trim() === '') {
+      alert('任务内容不得为空！')
+    }
+    let value = event.target.value
+    event.target.value = ''
     let newTodo = {
-      title: event.target.value,
-      complete: false,
-      deleted: false
+      title: value,
+      status: event.target.dataset.y === 'van' ? 'completed' : '',
+      deleted: event.target.dataset.x === 'imp'
     }
     TodoModel.create(newTodo, (id) => {
       newTodo.id = id
       this.state.todoList.push(newTodo)
       this.setState({
+        ...this.state,
         newTodo: '',
         todoList: this.state.todoList
       })
@@ -49,6 +58,7 @@ export default class App extends Component {
 
   changeTitle = (event) => {
     this.setState({
+      ...this.state,
       newTodo: event.target.value,
       todoList: this.state.todoList
     })
@@ -56,6 +66,7 @@ export default class App extends Component {
 
   toggle = (todo) => {//勾选（更新）操作
     let oldStatus = todo.status
+    console.log(oldStatus)
     todo.status = todo.status === 'completed' ? '' : 'completed'
     TodoModel.update(todo, () => {
       this.setState(this.state)
@@ -66,8 +77,8 @@ export default class App extends Component {
   }
 
   delete = (todo) => {//删除操作
-    TodoModel.destroy(todo.id, () => {
-      todo.deleted = true
+    todo.deleted = !todo.deleted
+    TodoModel.update(todo, () => {
       this.setState(this.state)
     })
   }
@@ -77,6 +88,13 @@ export default class App extends Component {
       ...this.state,
       user
     })
+    if (user) {
+      TodoModel.getByUser(user, (todos) => {
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.todoList = todos
+        this.setState(stateCopy)
+      })
+    }
   }
 
   signOut = () => {//调用登出API，并重置state
@@ -84,25 +102,101 @@ export default class App extends Component {
     this.stateUpdate({})
   }
 
+  changeOption = (e) => {
+    this.setState({
+      ...this.state,
+      option: e.target.dataset.key
+    })
+  }
+
+
+
   render() {
-    let todos = this.state.todoList.filter(item => !item.deleted).map((item, index) => {
+    // 
+    let doingTodos = this.state.todoList.filter(item => item.status === '').map((item, index) => {
       return (
         <li key={index}>
           <TodoItem todo={item} onToggle={this.toggle} onDelete={this.delete} />
         </li>)
     })
-    return (
+    let impTodos = this.state.todoList.filter(item => item.deleted).map((item, index) => {
+      return (
+        <li key={index}>
+          <TodoItem todo={item} onToggle={this.toggle} onDelete={this.delete} />
+        </li>)
+    })
+    let vanTodos = this.state.todoList.filter(item => item.status === 'completed').map((item, index) => {
+      return (
+        <li key={index}>
+          <TodoItem todo={item} onToggle={this.toggle} onDelete={this.delete} />
+        </li>)
+    })
+
+    let doing = (
       <div className="App">
-        <Aside username={this.state.user.username}/>
+        <Aside user={this.state.user} signOut={this.signOut} onChange={this.changeOption} />
         <div className="App-header">
-          <TodoInput content={this.state.newTodo}
-            onSubmit={this.addTodo}
-            onChange={this.changeTitle} />
+          <h1>进行中的事项</h1>
+          <span>当前有 {doingTodos.length} 条任务正在进行中</span>
         </div>
         <ol>
-          {todos}
+          {doingTodos}
+          <li>
+            <TodoInput content={this.state.newTodo}
+              onSubmit={this.addTodo}
+              onChange={this.changeTitle} />
+          </li>
         </ol>
-        {this.state.user.id ? null : <UserDialog stateUpdate={this.stateUpdate} />}
+      </div>)
+    let important = (
+      <div className="App">
+        <Aside user={this.state.user} signOut={this.signOut} onChange={this.changeOption} />
+        <div className="App-header">
+          <h1>重要事项</h1>
+          <span>当前有 {impTodos.length} 条重要任务</span>
+        </div>
+        <ol>
+          {impTodos}
+          <li>
+            <TodoInput content={this.state.newTodo}
+              onSubmit={this.addTodo}
+              onChange={this.changeTitle}
+              important={true} />
+          </li>
+        </ol>
+      </div>)
+    let van = (
+      <div className="App">
+        <Aside user={this.state.user} signOut={this.signOut} onChange={this.changeOption} option={this.state.option} />
+        <div className="App-header">
+          <h1>已完成的事项</h1>
+          <span>当前有 {vanTodos.length} 条任务已完成</span>
+        </div>
+        <ol>
+          {vanTodos}
+          <li>
+            <TodoInput content={this.state.newTodo}
+              onSubmit={this.addTodo}
+              onChange={this.changeTitle}
+              van={true} />
+          </li>
+        </ol>
+      </div>)
+    let judge = () => {
+      switch (this.state.option) {
+        case 'important':
+          return important
+        case 'van':
+          return van
+        default:
+          return doing
+      }
+    }
+    return (
+      <div>
+        {this.state.user.id ?
+          judge()
+          : <UserDialog stateUpdate={this.stateUpdate} />}
       </div>
     )
 
