a14505f5c80e669dc65e673f61429a9e3fb249c3
FanKaiqiang <fankaiqiang001@gmail.com>
2019-02-03 18:44:39 +0800

    各种状态下的input提交结果的处理

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../style/TodoInput.scss'
 
 
-function submit(props, e) {
-  if (e.key === 'Enter') {
-    props.onSubmit(e)
+export default class TodoInput extends Component {
+  submit=(e)=> {
+    if (e.key === 'Enter') {
+      this.props.onSubmit(e)
+    }
   }
-}
-
-function changeTitle(props, e) {
-  props.onChange(e)
-}
-export default function TodoInput(props) {
-  return (
-    <input type="text" className="TodoInput"
-      defaultValue={props.content}
-      onKeyPress={submit.bind(null, props)}
-      onChange={changeTitle.bind(null, props)} />
-  )
+  render() {
+    return (
+      <div className="inputArea">
+        <svg className="icon" viewBox="0 0 1024 1024" ><path d="M1024 448H576V0H448v448H0v128h448v448h128V576h448z" p-id="1951"></path></svg>
+        <input type="text"
+          defaultValue={this.props.content}
+          onKeyPress={this.submit}
+          onChange={this.props.onChange}
+          placeholder="在此处添加任务，按回车提交"
+          data-x={this.props.important?"imp":''}
+          data-y={this.props.van?"van":''}/>
+      </div>
+    )
+  }
 }
